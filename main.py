8f3cee372e3cb9392d8d487b14d74d681ea1a46f
AlexxxTyurin <33897009+AlexxxTyurin@users.noreply.github.com>
2019-08-15 16:54:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from functions import *
 
-data = np.array(pd.read_csv("/Users/alextyurin/Desktop/pycharm_projects/recognition/train.csv"))
+data = np.array(pd.read_csv("/Users/vladikturin/Desktop/pycharm_projects/recognition/train.csv"))
 
 Y = np.array(data[:, 0])
 
 X = np.array(data[:, 1:])
 m = X.shape[0]
 X = np.reshape(X, [m, 28, 28, 1])
-W = np.random.randn(28, 28, 3, 1)
-print(X.shape)
+W = np.random.randn(5, 5, 1)
+
+print(W.shape)
 
 hparams = {'stride': 1, 'padding': 2}
+result = conv_forward(X, W, 0, hparams)
+print(result.shape)
 
-X = zero_pad(X, 2)
-print(X.shape)
-# print(conv_forward(X, W, 0, hparams))
+result = pool(result, {'f': 2, 'stride': 2})
+print(result.shape)
 
 
 
